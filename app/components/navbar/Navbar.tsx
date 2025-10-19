4b8881c0ae523cf2d812927336b1ee5a98771378
Sumit uniyal <suniyal141@gmail.com>
2025-10-19 23:52:36 +0530

    social signin

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,34 +1,38 @@
-'use client'
+'use client';
 
-import React from 'react'
-import Containers from '../Containers'
-import Logo from './Logo'
-import Search from './Search'
-import UserMenu from './UserMenu'
+import React from 'react';
+import Containers from '../Containers';
+import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
+import { User } from '@prisma/client';
 
-
-const Navbar = () => {
-  return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-        <div className='py-4 border-b-[1px]'>
-            <Containers>
-                <div className="grid grid-cols-3 items-center">
-                    <div className="justify-self-start">
-                        <Logo />
-                    </div>
-
-                    <div className="justify-self-center">
-                        <Search />
-                    </div>
-
-                    <div className="justify-self-end">
-                        <UserMenu />
-                    </div>
-                </div>
-            </Containers>
-        </div>
-    </div>
-  )
+interface NavbarProps {
+  currentuser?: User | null;
 }
 
-export default Navbar
+const Navbar: React.FC<NavbarProps> = ({ currentuser }) => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Containers>
+          <div className="grid grid-cols-3 items-center">
+            <div className="justify-self-start">
+              <Logo />
+            </div>
+
+            <div className="justify-self-center">
+              <Search />
+            </div>
+
+            <div className="justify-self-end">
+              <UserMenu currentuser={currentuser} />
+            </div>
+          </div>
+        </Containers>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
