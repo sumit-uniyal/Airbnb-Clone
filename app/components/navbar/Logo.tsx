34be3beef322134c942b88d00792ac715303fa3c
Sumit uniyal <suniyal141@gmail.com>
2025-10-20 23:42:01 +0530

    category section

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,19 +1,21 @@
-'use client'
+'use client';
 
-import { useRouter } from "next/navigation"
-import Image from "next/image"
-
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const Logo = () => {
-  return (
-    <Image 
-        alt="Logo" 
-        className="hidden md:block cursor-pointer" 
-        height="100" 
-        width='100'
-        src='/images/logo.png' 
-    />
-  )
-}
+  const router = useRouter();
 
-export default Logo
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      height="100"
+      width="100"
+      src="/images/logo.png"
+    />
+  );
+};
+
+export default Logo;
